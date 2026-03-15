c82d1465fdd431a81d40ceed01e434973fa7b0b1
laoer <qiu_110chen@163.com>
2026-03-15 21:32:24 +0800

    删除老项目的参考代码

--- 3 月 15 日/app/sampling-tasks/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="min-h-screen bg-background flex items-center justify-center">
-      <div className="text-muted-foreground">加载中...</div>
-    </div>
-  )
-}
